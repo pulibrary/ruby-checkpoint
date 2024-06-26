8fd6ada4292f6b5aca0fd6e5a248b2dd7efb3ba4
Vivian Ha <vh8154@princeton.edu>
2024-06-26 13:49:28 -0400

    wrote related class and adjusted tests to accommodate class

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -4,8 +4,14 @@ require_relative '../house.rb'
 
 describe House do
     it 'returns the house that jack built rhyme' do
-        rhyme = House.new()
-        test_file = File.open('test_text.txt')
+        house = House.new('house.txt')
+        rhyme = house.rhyme
+
+        test_file = File.read('test_text.txt')
+
+        # puts "Test File:\n" + test_file
+        # puts "-" * 80
+        # puts "Rhyme:\n" + rhyme
         
         expect(rhyme).to match(test_file)
     end
